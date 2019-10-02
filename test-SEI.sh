5b1ad4b8ee2d218897853b47c531d4b806ec2a4d
Doug Waldron <douglas.waldron@dnr.ga.gov>
2019-10-02 08:42:26 -0400

    Add disaster recovery server tests

--- test-SEI.sh
+++ test-SEI.sh
@@ -1,15 +1,23 @@
 #!/bin/dash
 echo "Testing SEI servers"
-echo "Testing SEI servers" > output
+echo "░▒▓█ Testing SEI servers" > output
 
 ./testssl.sh -b >> output
 
+echo "░▒▓█ Testing 10.149.210.226" >> output
 ./testssl.sh -p -q 10.149.210.226 >> output
+echo "░▒▓█ Testing 10.149.210.227" >> output
 ./testssl.sh -p -q 10.149.210.227 >> output
+echo "░▒▓█ Testing 10.149.210.228" >> output
 ./testssl.sh -p -q 10.149.210.228 >> output
+echo "░▒▓█ Testing 10.149.211.66" >> output
 ./testssl.sh -p -q 10.149.211.66 >> output
+echo "░▒▓█ Testing 10.149.211.67" >> output
 ./testssl.sh -p -q 10.149.211.67 >> output
+echo "░▒▓█ Testing 10.149.211.68" >> output
 ./testssl.sh -p -q 10.149.211.68 >> output
+echo "░▒▓█ Testing 10.50.33.226" >> output
+./testssl.sh -p -q 10.50.33.226 >> output
 
 echo "Generating output file"
 
@@ -17,6 +25,6 @@ cat output | ~/scripts/scripts/ansi2html.sh > SEI-SSL-tests.html
 
 echo "Copying to E:/"
 
-cp SEI-SSL-tests.html /mnt/e/docs/testssl
+cp SEI-SSL-tests.html /mnt/e/docs/testssl-results
 
 echo "Done"
